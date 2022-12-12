14fa705e9b1188271e2c0ae317463b173cbce611
Dines <lozindenis6@gmail.com>
2022-12-12 21:32:22 +0200

    Create work "№2"

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ class Post:
     scale = ''
     type = ''
     def __init__(self):
-        print(f"created objects of car:")
-        self.name = "TOYOTA Springs"
-        self.position = 'Danila Galickiy street'
+        print(f"created objects of post:")
+        self.name = "Ukrainian boar"
+        self.position = 'Khreshchatyk'
         self.scale = 'big'
         self.type = 'free'
     def ShowOn(self):
         print(f"name: {self.name} \nposition: {self.position} \nscale: {self.scale} \ntype: {self.type}")
     def __del__(self):
-        print("deleted objects of car")
+        print("deleted objects of post")
 if __name__ == '__main__':
     post = Post()
     post.ShowOn()
